1fc512b6f1a901aed380ba13aa0e69a853d5f322
priya-upadhyay <priyaupadhyay.260@gmail.com>
2017-02-21 18:15:56 +0530

    new commit

--- app/src/main/java/com/temenos/dshubhamrajput/genericnet/AcctStmtActivity.java
+++ app/src/main/java/com/temenos/dshubhamrajput/genericnet/AcctStmtActivity.java
@@ -9,43 +9,23 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
 import android.os.Bundle;
 import android.view.View;
-
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.ImageButton;
 
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-
-import java.util.Locale;
-import android.view.View.OnClickListener;
-
-import static android.R.id.edit;
-import static android.support.v7.appcompat.R.id.text;
-import static com.temenos.dshubhamrajput.genericnet.R.id.image;
-
-import java.util.Calendar;
-import android.os.Bundle;
-import android.app.DatePickerDialog;
-import android.app.Dialog;
-import android.app.TimePickerDialog;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentActivity;
-import android.text.format.DateFormat;
-import android.view.View;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.TimePicker;
 import android.widget.Toast;
 
 
 public class AcctStmtActivity extends AppCompatActivity {
     static EditText DateEdit;
     static EditText DateEdit2;
+    static Context context;
     public static int year1,year,month1,month,day1,day;
+    static int diff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,11 +53,6 @@ public class AcctStmtActivity extends AppCompatActivity {
 
 
     }
-
-
-
-
-
     public void showDatePickerDialog(View v) {
         DialogFragment newFragment = new DatePickerFragment();
         newFragment.show(getSupportFragmentManager(), "datePicker");
@@ -86,23 +61,36 @@ public class AcctStmtActivity extends AppCompatActivity {
     public static class DatePickerFragment extends DialogFragment implements
             DatePickerDialog.OnDateSetListener {
 
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
+        @NonNull
+        public Dialog onCreateDialog(@NonNull Bundle savedInstanceState) {
+
             // Use the current date as the default date in the picker
             final Calendar c = Calendar.getInstance();
             year1 = c.get(Calendar.YEAR);
            month1 = c.get(Calendar.MONTH);
-             day1 = c.get(Calendar.DAY_OF_MONTH);
+            day1 = c.get(Calendar.DAY_OF_MONTH);
+         //   System.out.println("From Yr,Mnth,Day"+" "+year1+" "+month1+" "+day1);
 
 
-            // Create a new instance of DatePickerDialog and return it
+            // Create a new instance of D0atePickerDialog and return it
+            //DatePickerDialog.OnDateSetListener listener;
             return new DatePickerDialog(getActivity(), this, year1, month1, day1);
         }
 
         public void onDateSet(DatePicker view, int year1, int month1, int day1) {
-            // Do something with the date chosen by the user
-            //finddiff();
+            System.out.println("From Yr,Mnth,Day"+" "+year1+" "+(month1+1)+" "+day1);
+            diff=(month1+1)-(month+1);
+            System.out.println("Difference between months"+""+diff);
+            if(year1-year == 0)
+            {
+
+                if(!(diff>=6)) {
+                    getdiff(context);
+
+                }
+            }
             DateEdit.setText(day1 + "/" + (month1 + 1) + "/" + year1);
+
         }
 
     }
@@ -115,38 +103,43 @@ public class AcctStmtActivity extends AppCompatActivity {
     public static class DatePickerFragment2 extends DialogFragment implements
             DatePickerDialog.OnDateSetListener {
 
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
+        @NonNull
+        public Dialog onCreateDialog(@NonNull Bundle savedInstanceState) {
             // Use the current date as the default date in the picker
             final Calendar c = Calendar.getInstance();
              year = c.get(Calendar.YEAR);
           month = c.get(Calendar.MONTH);
           day = c.get(Calendar.DAY_OF_MONTH);
-
+            //System.out.println("To Yr,Mnth,Day"+" "+year+" "+month+" "+day);
             // Create a new instance of DatePickerDialog and return it
             return new DatePickerDialog(getActivity(), this, year, month, day);
+
         }
 
         public void onDateSet(DatePicker view, int year, int month, int day) {
+            System.out.println("To Yr,Mnth,Day"+" "+year+" "+(month+1)+" "+day);
+
             // Do something with the date chosen by the user
-//            if(year1-year == 0)
-//            {
-//                if(!(month1-month <=6)) {
-//                    Toast toast = Toast.makeText(getApplicationContext(),"Test", Toast.LENGTH_SHORT);
-//                    toast.show();
-//                }
-//            }
+
+
             DateEdit2.setText(day + "/" + (month + 1) + "/" + year);
+
         }
 
     }
 
 
     @Override
-    public boolean onSupportNavigateUp(){
+    public boolean onSupportNavigateUp() {
         finish();
         return true;
     }
+
+public static void getdiff(Context context)
+{
+    Toast toast = Toast.makeText(context,"Test", Toast.LENGTH_SHORT);
+                    toast.show();
+}
 
 }
 
