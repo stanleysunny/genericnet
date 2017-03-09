dc9f4631d6f84ba404bc40f139faa23b986e64f7
stanleysunny <95stans@gmail.com>
2017-03-09 14:38:42 +0530

    Added back functionality to funds transfer page

--- app/src/main/java/com/temenos/dshubhamrajput/genericnet/TransferBwAccounts.java
+++ app/src/main/java/com/temenos/dshubhamrajput/genericnet/TransferBwAccounts.java
@@ -2,12 +2,25 @@ package com.temenos.dshubhamrajput.genericnet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class TransferBwAccounts extends AppCompatActivity {
+public class TransferBwAccts extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transfer_bw_accts);
+        getSupportActionBar().setTitle("Account Transfer");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
