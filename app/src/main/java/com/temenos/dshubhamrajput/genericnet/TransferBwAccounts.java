3cc465a949ef08388066f941dcd84ad46d057dee
stanleysunny <95stans@gmail.com>
2017-03-09 14:39:37 +0530

    Made corrections

--- app/src/main/java/com/temenos/dshubhamrajput/genericnet/TransferBwAccounts.java
+++ app/src/main/java/com/temenos/dshubhamrajput/genericnet/TransferBwAccounts.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-public class TransferBwAccts extends AppCompatActivity {
+public class TransferBwAccounts extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
