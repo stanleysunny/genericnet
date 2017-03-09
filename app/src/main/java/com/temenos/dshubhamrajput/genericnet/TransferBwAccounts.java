e2fb9969e9cab858802663109ace433ff863d119
stanleysunny <95stans@gmail.com>
2017-03-09 14:19:28 +0530

    Funds Transfer page

---
+++ app/src/main/java/com/temenos/dshubhamrajput/genericnet/TransferBwAccounts.java
@@ -0,0 +1,13 @@
+package com.temenos.dshubhamrajput.genericnet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TransferBwAccounts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transfer_bw_accts);
+    }
+}
