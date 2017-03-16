1652e1a564c2286d73bdfb647db39a4a10e28d3a
stanleysunny <95stans@gmail.com>
2017-03-16 15:25:17 +0530

    blank confimation xml

---
+++ app/src/main/java/com/temenos/dshubhamrajput/genericnet/ConfirmPage.java
@@ -0,0 +1,13 @@
+package com.temenos.dshubhamrajput.genericnet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConfirmPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_page);
+    }
+}
