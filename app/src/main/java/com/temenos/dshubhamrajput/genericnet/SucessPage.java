6950e15083d8c98a1b2baee122cddc589b06de2f
stanleysunny <95stans@gmail.com>
2017-03-17 13:11:05 +0530

    Success page creation

---
+++ app/src/main/java/com/temenos/dshubhamrajput/genericnet/SucessPage.java
@@ -0,0 +1,15 @@
+package com.temenos.dshubhamrajput.genericnet;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SucessPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sucess_page);
+
+    }
+}
