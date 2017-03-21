9c4e59071b9d00a21fe09676df26cd13d6fbd0f8
KavyaC <ckavya005@gmail.com>
2017-03-21 12:01:42 +0530

    Add beneficiary Validation
    Beneficiary Management Validtion and UI updated.

--- app/src/main/java/com/temenos/dshubhamrajput/genericnet/Addbeneficiary.java
+++ app/src/main/java/com/temenos/dshubhamrajput/genericnet/Addbeneficiary.java
@@ -5,8 +5,6 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -17,11 +15,20 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
-public class Addbeneficiary extends AppCompatActivity  {
+public class Addbeneficiary extends AppCompatActivity {
 
-    Matcher matcher;
-    public String intentData="internal";
-    public  Intent commit;
+
+    public String intentData = "internal";
+    public Intent commit;
+    CheckBox withinbank1;
+    CheckBox neft1;
+    TextView ifscTextview;
+    EditText ifscEtext;
+    EditText benAccNo;
+    EditText accNoCheck;
+    EditText emailUser;
+    ImageView helpicon;
+    EditText nickName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,21 +37,15 @@ public class Addbeneficiary extends AppCompatActivity  {
         getSupportActionBar().setTitle("Add beneficiary");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        final CheckBox withinbank1=(CheckBox)findViewById(R.id.withinbank);
-        final CheckBox neft1=(CheckBox)findViewById(R.id.neft);
-        final TextView ifscTextview = (TextView) findViewById(R.id.textView5);
-        final EditText ifscEtext = (EditText) findViewById(R.id.Ifsc);
-        final EditText benAccNo=(EditText)findViewById(R.id.BenAccNo) ;
-        final EditText accNoCheck=(EditText)findViewById(R.id.ReenterAccNo) ;
-        final EditText emailUser=(EditText)findViewById(R.id.Email) ;
-        final ImageView helpicon=(ImageView) findViewById(R.id.help_icon);
-        final String ifscChecker="/^[A-Z]{4}[0][0-9A-Z]{6}$/";
-
-//        Pattern pattern = Pattern.compile(ifscChecker);
-        final String ifsc_matcher=ifscEtext.getText().toString();
-//        matcher = pattern.matcher(ifscEtext.getText().toString());
-
-
+        withinbank1 = (CheckBox) findViewById(R.id.withinbank);
+        neft1 = (CheckBox) findViewById(R.id.neft);
+        ifscTextview = (TextView) findViewById(R.id.textView5);
+        ifscEtext = (EditText) findViewById(R.id.Ifsc);
+        benAccNo = (EditText) findViewById(R.id.BenAccNo);
+        accNoCheck = (EditText) findViewById(R.id.ReenterAccNo);
+        emailUser = (EditText) findViewById(R.id.Email);
+        helpicon = (ImageView) findViewById(R.id.help_icon);
+        nickName = (EditText) findViewById(R.id.NickName);
 
 
         withinbank1.setOnClickListener(new View.OnClickListener() {
@@ -52,12 +53,12 @@ public class Addbeneficiary extends AppCompatActivity  {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                if(withinbank1.isChecked()){
+                if (withinbank1.isChecked()) {
                     neft1.setChecked(false);
                     ifscTextview.setVisibility(View.INVISIBLE);
                     ifscEtext.setVisibility(View.INVISIBLE);
                     helpicon.setVisibility(View.INVISIBLE);
-                    intentData="internal";
+                    intentData = "internal";
 
                 }
             }
@@ -68,85 +69,57 @@ public class Addbeneficiary extends AppCompatActivity  {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                if(neft1.isChecked()){
+                if (neft1.isChecked()) {
                     withinbank1.setChecked(false);
                     ifscTextview.setVisibility(View.VISIBLE);
                     ifscEtext.setVisibility(View.VISIBLE);
                     helpicon.setVisibility(View.VISIBLE);
-                    intentData="external";
+                    intentData = "external";
 
                 }
             }
         });
-        accNoCheck.addTextChangedListener(new TextWatcher()  {
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                accNoCheck.setError(null);
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                accNoCheck.setError(null);
-            }
-
-            @Override
-            public void afterTextChanged(Editable s)  {
+        accNoCheck.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            public void onFocusChange(View v, boolean hasFocus) {
                 if (!(accNoCheck.getText().toString().equals(benAccNo.getText().toString()))) {
                     accNoCheck.setError("Account numbers don't match");
-                } else {
-                    accNoCheck.setError(null);
+                } else if (((accNoCheck.getText().toString()).matches(""))) {
+                    accNoCheck.setError("This field cannot be left blank");
+                }
+
+            }
+
+        });
+        emailUser.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    final String email = emailUser.getText().toString();
+                    if (!(emailValidator(email))) {
+                        emailUser.setError("Enter a valid email id");
+                    } else if ((emailUser.getText().toString()).matches("")) {
+                        emailUser.setError(null);
+                    }
                 }
             }
         });
-        emailUser.addTextChangedListener(new TextWatcher()  {
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                emailUser.setError(null);
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                emailUser.setError(null);
-            }
-
-            @Override
-            public void afterTextChanged(Editable s)  {
-                final String email = emailUser.getText().toString();
-                if (!(emailValidator(email))) {
-                    emailUser.setError("Enter a valid email id");
-                } else {
-                    emailUser.setError(null);
+        ifscEtext.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus) {
+                    String ifsc = ifscEtext.getText().toString();
+                    boolean check = ifscMatcher(ifsc);
+                    if (!check) {
+                        ifscEtext.setError("IFSC is a 11 digit alpha numeric string");
+                    }
+                    else if (ifscEtext.getText().toString().matches("")) {
+                        ifscEtext.setError("This field cannot be left blank");
+                    }
                 }
             }
-        });
-        ifscEtext.addTextChangedListener(new TextWatcher()  {
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                ifscEtext.setError(null);
-            }
-
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                ifscEtext.setError(null);
-            }
-
-            @Override
-            public void afterTextChanged(Editable s)  {
-                if(!(ifsc_matcher.matches(ifscChecker)))
-                    ifscEtext.setError("IFSC should be a 11 character alpha numeric string");
-                else
-                    ifscEtext.setError(null);
-            }
-
-
         });
     }
 
-    public boolean emailValidator(String email)
-    {
+    public boolean emailValidator(String email) {
         Pattern pattern;
         Matcher matcher;
         final String EMAIL_PATTERN = "^[_A-Za-z0-9-]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
@@ -154,8 +127,17 @@ public class Addbeneficiary extends AppCompatActivity  {
         matcher = pattern.matcher(email);
         return matcher.matches();
     }
-    public void showHelpText(View v)
-    {
+
+    public boolean ifscMatcher(String ifsc) {
+        Pattern pattern;
+        Matcher matcher;
+        final String ifscChecker = "^[A-Z]{4}\\d{7}$" ;
+        pattern = Pattern.compile(ifscChecker);
+        matcher = pattern.matcher(ifsc);
+        return matcher.matches();
+    }
+
+    public void showHelpText(View v) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
         builder.setMessage(R.string.ifsc_help)
@@ -163,23 +145,37 @@ public class Addbeneficiary extends AppCompatActivity  {
                 .setCancelable(false)
                 .setPositiveButton("OK", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                       //nothing is done
+                        //nothing is done
                     }
                 });
         AlertDialog alert = builder.create();
         alert.show();
     }
-    public boolean onSupportNavigateUp(){
+
+    public boolean onSupportNavigateUp() {
         finish();
         return true;
     }
-    public void ButtonClicked(View V)
-    {
 
-            commit=new Intent(this,ConfirmPage.class);
-            commit.putExtra("getintent",intentData);
-            startActivity(commit);
+    public void ButtonClicked(View V) {
+        if(intentData.equals("external")) {
+            if ((ifscEtext.getText().toString()).matches(""))
+                ifscEtext.setError("This field cannot be left blank");
+        }
+        if ((benAccNo.getText().toString()).matches(""))
+            benAccNo.setError("This field cannot be left blank");
+        if ((accNoCheck.getText().toString()).matches(""))
+            accNoCheck.setError("This field cannot be left blank");
+        if ((nickName.getText().toString()).matches(""))
+            nickName.setError("This field cannot be left blank");
+        if (((benAccNo.getError() == null) && (accNoCheck.getError() == null) && (emailUser.getError() == null) && (nickName.getError() == null) && (ifscEtext.getError() == null)))
+            {
+                System.out.print("Intent should  work");
+                commit = new Intent(this, ConfirmPage.class);
+                commit.putExtra("getintent", intentData);
+                startActivity(commit);
+            }
+
+        }
 
     }
-
-}
