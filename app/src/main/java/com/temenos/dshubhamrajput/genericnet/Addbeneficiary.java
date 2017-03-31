0af098d635cecefb4f7ee0f5b0867f0576ff8026
priya-upadhyay <priyaupadhyay.260@gmail.com>
2017-03-31 10:34:31 +0530

    progress bar  in add ben

--- app/src/main/java/com/temenos/dshubhamrajput/genericnet/Addbeneficiary.java
+++ app/src/main/java/com/temenos/dshubhamrajput/genericnet/Addbeneficiary.java
@@ -429,6 +429,10 @@ public class Addbeneficiary extends AppCompatActivity {
 
         @Override
         protected void onPreExecute() {
+            progressDialog= new ProgressDialog(Addbeneficiary.this);
+            progressDialog.setMessage("Please wait...");
+            progressDialog.show();
+            progressDialog.setCancelable(false);
             super.onPreExecute();
         }
 
